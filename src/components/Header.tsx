7b0f6b9aded9c302108255a95cc421d43d597e1d
hermesonbastos <hermesonbastos@gmail.com>
2023-09-06 09:09:25 -0300

    feat: first commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,35 @@
+"use client";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+} from "@/components/ui/navigation-menu";
+import { Button } from "./ui/button";
+
+function Header() {
+  return (
+    <header className="flex w-full justify-between pl-11 pr-11 px-5 py-5">
+      <h1 className="font-bold">PET-SI</h1>
+      <div className="flex gap-1">
+        <NavigationMenu>
+            <NavigationMenuList>
+            <NavigationMenuItem>
+                <NavigationMenuTrigger>Projetos</NavigationMenuTrigger>
+                <NavigationMenuContent className="p-2">
+                <NavigationMenuLink>Link</NavigationMenuLink>
+                </NavigationMenuContent>
+            </NavigationMenuItem>
+            </NavigationMenuList>
+        </NavigationMenu>
+        <Button>Login</Button>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
